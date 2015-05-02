240975f48f4bd4172d6a58a86a5ce2c368aa26ce
israel <gears3292@gmail.com>
2015-05-01 23:29:04 -0600

    added load.js and began work on html for image galleries for individual tabs.

---
+++ js/load.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#tab-1").load("html/graphic_design.html");
+    $("#tab-2").load("html/motion_design.html");
+    $("#tab-3").load("html/uiux_design.html");
+    $("#tab-4").load("html/typography.html");
+    $("#tab-5").load("html/illustration.html");
+});
+
